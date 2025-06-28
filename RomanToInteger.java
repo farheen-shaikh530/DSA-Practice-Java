bbb0876b7fa7dcceebf7ac4c6cc513d3d019d9f6
Farheen Shaikh <farheenshaikh05091999@gmail.com>
2025-06-27 23:35:06 -0700

    Add Roman to Integer solution

---
+++ RomanToInteger.java
@@ -0,0 +1,39 @@
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = value(s.charAt(i));
+
+            if (curr < prev) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+
+            prev = curr;
+        }
+
+        return result;
+    }
+
+    private static int value(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+
+    public static void main(String[] args) {
+        String roman = "MCMXCIV";
+        System.out.println("The integer value of " + roman + " is " + romanToInt(roman));
+    }
+
+}
