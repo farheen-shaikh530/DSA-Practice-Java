90f5b10a6511254414bfe30bb846ca106c153c8a
Farheen Shaikh <farheenshaikh05091999@gmail.com>
2025-11-26 00:38:13 -0800

    Time: 3 ms (58.97%), Space: 46.3 MB (25.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-
         int[] count = new int[26];
-
-        // count magazine chars
         for(char c : magazine.toCharArray()){
             count[c - 'a']++;
         }
