f6fd02c4aca13d4d98aa5b5c47d78543c9f9a107
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-01-07 19:50:35 -0800

    Time: 592 ms (25.51%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,21 @@
+Select 
+
+ROUND( 
+
+    COUNT(Distinct a.player_id) / 
+
+
+    (Select COUNT(Distinct player_id) from Activity) ,2) as fraction
+
+from activity a
+
+join (Select player_id, MIN(event_date) AS first_login 
+FROM Activity
+    GROUP BY player_id
+) f
+
+ON a.player_id = f.player_id
+   AND a.event_date = DATE_ADD(f.first_login, INTERVAL 1 DAY);
+
+
+
