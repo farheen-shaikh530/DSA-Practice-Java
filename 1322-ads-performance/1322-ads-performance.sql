fbea82a304835fd3e99b09922d7205a9a782723c
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-01-26 23:34:45 -0800

    Time: 528 ms (60.5%), Space: 0B (100%) - LeetHub

---
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -0,0 +1,13 @@
+SELECT
+    ad_id,
+    ROUND(
+        IFNULL(
+            SUM(action = 'Clicked') * 100.0 /
+            NULLIF(SUM(action IN ('Clicked', 'Viewed')), 0),
+            0
+        ),
+        2
+    ) AS ctr
+FROM Ads
+GROUP BY ad_id
+ORDER BY ctr DESC, ad_id ASC;
