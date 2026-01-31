966c4bf2cc5a288141108b31e45547b4d4f52977
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-01-31 00:21:47 -0800

    Time: 715 ms (18.28%), Space: 0B (100%) - LeetHub

---
+++ 0534-game-play-analysis-iii/0534-game-play-analysis-iii.sql
@@ -0,0 +1,10 @@
+Select a1.player_id,
+a1.event_date,
+SUM(a2.games_played) as games_played_so_far
+
+from Activity a1
+
+join Activity a2 on a1.player_id = a2.player_id
+and a2.event_date <= a1.event_date
+
+GROUP BY a1.player_id, a1.event_date;
