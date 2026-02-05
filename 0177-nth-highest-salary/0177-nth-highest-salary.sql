2cc7873ca242f39e8014993a6a94f83897f0a143
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-02-05 05:35:07 -0800

    Time: 453 ms (34.13%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,20 +1,12 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  DECLARE off INT;
+  SET off = N - 1;
 
-declare off int;
-SET off = N - 1;
-
-if N <= 0 then return null;
-END if;
-
-
-RETURN (
-
-Select distinct salary 
-from Employee
- order by salary desc 
- limit 1 
- OFFSET off
-
+  RETURN (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET off
   );
 END
