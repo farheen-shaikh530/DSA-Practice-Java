0a58f4ab0b80f3bcaf17c06e433407721489bc66
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-02-06 17:28:41 -0800

    Time: 651 ms (84.97%), Space: 0B (100%) - LeetHub

---
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -0,0 +1,17 @@
+    # Write your MySQL query statement below
+    Select  e.business_id 
+    from Events e 
+join 
+     (
+        select event_type, 
+        AVG(occurrences) as avg_occ 
+        from Events
+        group by  event_type
+    ) 
+     a
+
+on e.event_type = a.event_type
+WHERE e.occurrences > a.avg_occ
+GROUP BY e.business_id
+HAVING COUNT(DISTINCT e.event_type) >= 2;
+
