8cdf0e7a2c1b682327fb1a7cc71a1b2b752fdc83
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-02-09 00:27:50 -0800

    Time: 649 ms (67.45%), Space: 0B (100%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -7,6 +7,6 @@ join Customers c ON o.customer_id = c.customer_id
 
 group by o.customer_id, c.customer_name
 HAVING
-  SUM(o.product_name = 'A') > 0
+SUM(o.product_name = 'A') > 0
 AND SUM(o.product_name = 'B') > 0
 AND SUM(o.product_name = 'C') = 0;
