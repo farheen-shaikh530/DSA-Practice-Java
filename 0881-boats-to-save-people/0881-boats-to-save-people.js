c7a83856b63ddd00caf981fd75d30cb0c47ecdfc
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-02-12 00:41:32 -0800

    Time: 66 ms (80.84%), Space: 64.1 MB (14.7%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,18 @@
+
+function numRescueBoats(people, limit){
+    people.sort((a,b) => a - b);
+
+    let i = 0;
+    let j = people.length - 1;
+    let boats = 0;
+
+    while(i <= j){
+
+if(people[i] + people[j] <= limit) i++;
+
+j--;
+boats++;  
+
+    }
+    return boats;
+}
