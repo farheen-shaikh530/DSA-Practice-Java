1d6adea75b99dd8771c175e0d37f4b94dcc3a8aa
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-03-16 03:05:16 -0500

    Time: 345 ms (25.45%), Space: 0B (100%) - LeetHub

---
+++ 1445-apples-oranges/1445-apples-oranges.sql
@@ -0,0 +1,14 @@
+select 
+ s1.sale_date,
+s1.sold_num - s2.sold_num as diff
+
+ from Sales s1
+
+  join Sales s2 
+  on s1.sale_date = s2.sale_date
+
+  and s1.fruit = 'apples'
+  and s2.fruit ='oranges'
+
+ORDER BY
+s1.sale_date;
