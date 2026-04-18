a43c20af1ae7e84860222fd7f85dc85fe9f45f7e
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-04-18 16:54:41 -0700

    Time: 529 ms (65.49%), Space: 0B (100%) - LeetHub

---
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -0,0 +1,11 @@
+Select 
+
+ LEAST(from_id, to_id) as person1,
+ GREATEST(from_id, to_id) as person2,
+
+COUNT(*) as call_count,
+
+SUM(duration) as total_duration
+
+from Calls
+GROUP BY person1, person2
