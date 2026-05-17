880d9bc568c3ee9f35370fab336f03b051666b09
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-05-17 00:57:39 -0700

    Time: 1287 ms (5.64%), Space: 0B (100%) - LeetHub

---
+++ 2292-products-with-three-or-more-orders-in-two-consecutive-years/2292-products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -0,0 +1,16 @@
+with yearly_orders as(
+    select product_id,
+    YEAR(purchase_date) as purchase_year,
+    COUNT(*) as order_count
+    from Orders
+    group by product_id, YEAR(purchase_date)
+    having count(*) >= 3
+)
+
+select distinct y1.product_id
+from yearly_orders y1
+join yearly_orders y2
+
+on y1. product_id = y2.product_id
+and 
+y2.purchase_year = y1.purchase_year + 1;
