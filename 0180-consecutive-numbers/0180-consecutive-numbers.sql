5974843dde0ca243b03ad1cfa6b718e462890a8f
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-05-20 03:45:31 -0700

    Time: 544 ms (69.33%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,19 +1,13 @@
-SELECT DISTINCT num AS ConsecutiveNums
+Select distinct
+num as ConsecutiveNums
+from (
 
-FROM (
+    Select 
+    num,
+    LAG(num, 1) OVER (Order by id)  as prev1,
+    LAG(num, 2) OVER (Order by id) as prev2
+    from 
+    Logs
+) as t
 
-    SELECT
-
-        num,
-
-        LAG(num,1) OVER (ORDER BY id) AS prev1,
-
-        LAG(num,2) OVER (ORDER BY id) AS prev2
-
-    FROM Logs
-
-) t
-
-WHERE num = prev1
-
-AND num = prev2;
+where num = prev1 and num = prev2
