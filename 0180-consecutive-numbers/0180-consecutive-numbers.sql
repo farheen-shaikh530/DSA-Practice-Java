4f57ec1dc327ecb8094785f5071e1508c7ca173a
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-05-20 03:08:51 -0700

    Time: 621 ms (20.1%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,12 +1,13 @@
-Select 
-distinct l1.num 
-as ConsecutiveNums
+Select distinct 
 
-
+l1.num as ConsecutiveNums
 from Logs l1
-join Logs l2 on l1.id = l2.id -1
-join Logs l3 on l3.id = l1.id -1
+
+join Logs l2 
+on l1.id = l2.id - 1
+
+join Logs l3 
+on l1.id = l3.id - 2
 
 where l1.num = l2.num
 and l2.num = l3.num
-
