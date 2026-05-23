688af4fcd867b7e6e5207b08e39fefac072fc52c
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-05-23 05:41:50 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -2,7 +2,10 @@ Select
 e1.name
 
 from Employee e1
-join Employee e2 on e1.managerId = e2.id
+join Employee e2 
 
-group by e1.managerId, e1.name
-having count(*) >= 5;
+on e1.managerId = e2.id
+
+group by e1.managerId, e2.name
+
+HAVING COUNT(*) >= 5;
