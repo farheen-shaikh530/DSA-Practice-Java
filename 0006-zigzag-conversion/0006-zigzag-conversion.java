4cc94033dffb7cbb149b0a77ceb005b8f14494e5
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-05-27 20:20:21 -0700

    Time: 5 ms (53.75%), Space: 46.7 MB (36.94%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,57 +1,39 @@
 class Solution {
-
     public String convert(String s, int numRows) {
-
-        if (numRows == 1 || numRows >= s.length()) {
-
+        if(numRows == 1 || numRows >= s.length()){
             return s;
-
         }
 
-        StringBuilder[] rows = new StringBuilder[numRows];
+StringBuilder[] rows = new StringBuilder[numRows];
+for(int i = 0; i < numRows; i++){
+    rows[i] = new StringBuilder();
+}
 
-        for (int i = 0; i < numRows; i++) {
+int currentRow = 0;
+boolean goingDown = false;
 
-            rows[i] = new StringBuilder();
+for(char ch : s.toCharArray()){
+    rows[currentRow].append(ch);
 
-        }
+if(currentRow == 0 || currentRow == numRows - 1){
+    goingDown = !goingDown;
+}
 
-        int currentRow = 0;
+if(goingDown){
+    currentRow++;
+}
 
-        boolean goingDown = false;
-
-        for (char ch : s.toCharArray()) {
-
-            rows[currentRow].append(ch);
-
-            if (currentRow == 0 || currentRow == numRows - 1) {
-
-                goingDown = !goingDown;
-
-            }
-
-            if (goingDown) {
-
-                currentRow++;
-
-            } else {
-
-                currentRow--;
-
-            }
-
-        }
-
-        StringBuilder result = new StringBuilder();
-
-        for (StringBuilder row : rows) {
-
-            result.append(row);
-
-        }
-
-        return result.toString();
-
+else{
+    currentRow--;
+}
     }
 
+StringBuilder result = new StringBuilder();
+ for(StringBuilder row : rows){
+    result.append(row);
+
+ }
+
+return result.toString();
+    }
 }
