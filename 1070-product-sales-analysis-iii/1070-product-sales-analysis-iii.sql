93f5838ed09b9dd5796e49e41177d816b84534c5
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-06-11 16:54:51 -0600

    Time: 690 ms (79.24%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,17 +1,17 @@
-select s1.product_id, 
-s1.quantity,
-s1.year as first_year,
-s1.price
-from Sales s1
+select
+product_id,
+year as first_year,
+quantity,
+price
 
-join(
-select product_id, MIN(year) AS first_year
- from Sales group by product_id
+from Sales
 
+where (product_id, year) IN  (
+
+    Select 
+    product_id,
+    MIN(year)
+    FROM Sales
+
+    group by product_id
 )
-as f
-on s1.product_id = f.product_id
- AND s1.year = f.first_year;
-
-
-
