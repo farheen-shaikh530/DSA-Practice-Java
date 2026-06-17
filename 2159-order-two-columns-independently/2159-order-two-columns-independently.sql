91888229f00d0726373ee7fd1574f04e3c3c1124
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-06-17 16:10:17 -0600

    Time: 462 ms (47.15%), Space: 0B (100%) - LeetHub

--- 2159-order-two-columns-independently/2159-order-two-columns-independently.sql
+++ 2159-order-two-columns-independently/2159-order-two-columns-independently.sql
@@ -1,20 +1,26 @@
-WITH CTE1 as(
-    select first_col,
-    Row_number() over (order by first_col asc) as rn
+WITH first_sorted as (
+    Select
+    first_col,
+    ROW_NUMBER() OVER (ORDER BY first_col asc) as rn 
     from Data
 
 ),
 
- cte2 as (
-select second_col,
-Row_number() over (order by second_col desc) as rn
-from Data
+second_sorted as (
+    SELECT
+    second_col,
+        ROW_NUMBER() OVER (ORDER BY second_col desc) as rn 
+        from Data
+
 
 )
 
-select cte1.first_col,
-cte2.second_col
-from cte1
+SELECT 
+f.first_col,
+s.second_col
 
-join cte2
-on cte1.rn =   cte2.rn 
+from first_sorted f
+JOIN second_sorted s
+
+
+ON f.rn = s.rn;
