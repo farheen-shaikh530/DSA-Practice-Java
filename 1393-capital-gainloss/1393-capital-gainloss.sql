06d48e2d2ec5142e88a77b053790604bdf104485
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-06-20 15:32:40 -0600

    Time: 613 ms (14.38%), Space: 0B (100%) - LeetHub

---
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -0,0 +1,19 @@
+
+Select
+
+stock_name,
+SUM(
+
+    CASE 
+    WHEN
+    operation = 'Sell'
+    THEN price
+
+    ELSE -price
+    END
+
+) as capital_gain_loss
+
+FROM Stocks
+
+GROUP BY stock_name
