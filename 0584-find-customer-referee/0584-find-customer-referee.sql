d7ab14ea56e2a3d263959ab0aace820dd9bfd83c
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-06-26 16:30:37 -0600

    Time: 501 ms (52.65%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,5 @@
-# Write your MySQL query statement below
-Select name
-
-from Customer
-
-where referee_id is null 
-or referee_id <> 2
+SELECT
+name
+FROM Customer
+WHERE referee_id != 2
+OR referee_id IS NULL 
