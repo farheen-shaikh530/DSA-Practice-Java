66e73a6c96234895b74a9a3640081f113ff27676
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-06-28 16:11:47 -0600

    Time: 1205 ms (98.98%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,38 +1,28 @@
 (
-Select 
-u.name as results
 
-from MovieRating mr
+    SELECT 
+    u.name as results
+    FROM MovieRating mr
+    JOIN Users u on mr.user_id = u.user_id
+    GROUP BY mr.user_id
 
-join Users u
- on u.user_id = mr.user_id
+    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
+    LIMIT 1
 
-Group by u.name
-order by COUNT(*) desc, u.name asc
-LIMIT 1
 )
-
 UNION ALL
+ (
 
-(Select mv.title as results
-from MovieRating mr
+    SELECT m.title as results 
+    FROM MovieRating mr
+    JOIN Movies m on mr.movie_id = m.movie_id
 
-JOIN Movies mv on mv.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01' AND mr.created_at < '2020-03-01'
 
-where created_at between '2020-02-01' AND '2020-02-29'
-Group by mv.title
-order by avg(mr.rating) desc, mv.title asc
-limit 1
-);
+    GROUP BY mr.movie_id
 
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
 
+    LIMIT 1
 
-
-
-
-
-
-
-
-
-
+ );
