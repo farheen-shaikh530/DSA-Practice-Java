8e4303ac8a21906dc765b51f915860a6cc0ca7c6
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-07-06 14:10:03 -0600

    Time: 322 ms (57.98%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,16 +1,19 @@
 SELECT
 id,
-COUNT(*) as num
-FROM(
-
-    SELECT requester_id AS id FROM RequestAccepted
+COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id 
+    FROM RequestAccepted
 
     UNION ALL
 
-    SELECT accepter_id AS id FROM RequestAccepted
-
-) friends
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+) AS friends
 
 GROUP BY id
 ORDER BY num DESC
-LIMIT 1;
+
+Limit 1;
+
+    
