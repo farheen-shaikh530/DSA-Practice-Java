59c505e2b737b24316256ef45853f906cc04e90e
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-07-07 01:38:57 -0600

    Time: 268 ms (59.15%), Space: 0B (100%) - LeetHub

--- 0618-students-report-by-geography/0618-students-report-by-geography.sql
+++ 0618-students-report-by-geography/0618-students-report-by-geography.sql
@@ -1,17 +1,19 @@
-Select
-MAX(CASE WHEN continent = 'America' THEN name END) as America,
-MAX(CASE WHEN continent = 'Asia' THEN name END) as Asia,
-MAX(CASE WHEN continent = 'Europe' THEN name END) as Europe
+SELECT
 
-from (
+MAX(CASE WHEN continent = 'America' THEN name END) AS America,
+MAX(CASE WHEN continent = 'Asia' THEN name END) AS Asia,
 
-    Select 
-    name, 
+MAX(CASE WHEN continent = 'Europe' THEN name END ) AS Europe
+
+FROM 
+(
+
+    SELECT 
+    name,
     continent,
-    ROW_NUMBER() OVER (PARTITION BY continent ORDER BY name) as rn
-from Student
-    
+    ROW_NUMBER() OVER 
+    ( PARTITION BY continent ORDER BY name)  AS rn
+    FROM Student
 ) t
 
-Group by rn
-ORDER BY rn
+GROUP BY rn;
