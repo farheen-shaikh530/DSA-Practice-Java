126a66dabfcb85c2bd3779ab9f3763c81f72bc23
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-07-08 15:57:24 -0600

    Time: 570 ms (96.32%), Space: 0B (100%) - LeetHub

--- 1077-project-employees-iii/1077-project-employees-iii.sql
+++ 1077-project-employees-iii/1077-project-employees-iii.sql
@@ -6,10 +6,11 @@ FROM
     SELECT 
     p.project_id,
     p.employee_id,
-    RANK() OVER (PARTITION BY p.project_id
+    RANK() OVER 
+    (
+        PARTITION BY p.project_id
     ORDER BY e.experience_years DESC
-
-) AS rnk
+    ) AS rnk
 
     FROM Project p
     JOIN Employee e
