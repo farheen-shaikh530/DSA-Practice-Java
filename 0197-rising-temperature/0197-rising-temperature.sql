e2c2d55f366fe74b1c4c2d5570abbbb50e89005f
Farheen Shabbir Shaikh <farheen.s.shaikh05@gmail.com>
2026-07-09 15:01:25 -0600

    Time: 566 ms (17.73%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,9 +1,20 @@
-select w1.id
-from Weather w1
-
-join Weather w2 
 
 
- ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+SELECT
+w1.id
 
-where w1.temperature > w2.temperature
+FROM Weather w1
+
+JOIN Weather w2
+
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature
+
+ 
+
+
+
+
+
+
+
